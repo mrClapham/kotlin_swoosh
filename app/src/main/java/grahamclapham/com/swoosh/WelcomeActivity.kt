d03644c31d4870553fcc9517a32288f64e274425
Graham Clapham <grahamclapham@Grahams-new-MacBook-Pro.local>
2018-04-13 09:34:49 +0100

    Third screen started.

--- app/src/main/java/grahamclapham/com/swoosh/WelcomeActivity.kt
+++ app/src/main/java/grahamclapham/com/swoosh/WelcomeActivity.kt
@@ -1,12 +1,19 @@
 package grahamclapham.com.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBttn.setOnClickListener {
+            val leageIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leageIntent)
+        }
     }
 }
